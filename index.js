a286f27138528f02d086278695b9dc97f7d26a3e
AlexMinez <alex_minihan@outlook.com>
2022-04-05 13:20:09 +1000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string){
+    string = string.replaceAll( /[^A-Za-z0-9 '-]/g, '' )
+      return string
+    
+  }
+  static titleize (string){
+      let wordsThrough = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+      let result = []
+      let arr = string.split(" ")
+      for( let i = 0; i < arr.length; i++) {
+        if (i == 0){
+          result.push(this.capitalize(arr[i]))
+        }else {
+          if (wordsThrough.includes(arr[i])){
+            result.push(arr[i])
+          }else {
+            result.push(this.capitalize(arr[i]))
+          }
+        }
+      }
+      return result.join(" ")
+    }
+  }
